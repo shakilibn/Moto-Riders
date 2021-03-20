e554961b1f9593b20724f9530f44b2e0d32a800e
shakilibn <mdsh540@gmail.com>
2021-03-20 23:01:33 +0600

    hurrah...! google map added

--- src/components/SearchMap/SearchMap.js
+++ src/components/SearchMap/SearchMap.js
@@ -1,12 +1,46 @@
-import React from 'react';
-import map from '../../images/Map.png'
+// import React from 'react';
+// import map from '../../images/Map.png'
+// import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 
-const SearchMap = () => {
-    return (
-        <div>
-            <img className="img-fluid mt-2" src={map} alt=""/>
-        </div>
-    );
-};
+// const SearchMap = () => {
+//     return (
+//         <div>
+//             {/* <img className="img-fluid mt-2" src={map} alt=""/> */}
+//         </div>
+//     );
+// };
 
-export default SearchMap;
+// export default SearchMap;
+
+import React, { Component } from 'react';
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
+import './SearchMap.css'
+
+class SearchMap extends Component {
+    render() {
+        const containerStyle = {
+            position: 'relative',
+            width: '100%',
+            height: '600px'
+          }
+        return (
+            <div className="mt-2 map-container">
+                <Map style={containerStyle} google={this.props.google} zoom={14}>
+
+                    <Marker onClick={this.onMarkerClick}
+                        name={'Current location'} />
+
+                    <InfoWindow onClose={this.onInfoWindowClose}>
+                        
+                    </InfoWindow>
+                </Map>
+            </div>
+        );
+    }
+}
+
+// export default SearchMap;
+
+export default GoogleApiWrapper({
+    apiKey: ("AIzaSyDa24JhGbTIamrEbdha0x3IcDI0J7ylH7Y")
+  })(SearchMap)
